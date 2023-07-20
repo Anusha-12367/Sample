a0564554dd88b29d920ff7b5de2c46fbfc7e1b6b
ANUSHA <anushamohan03@gmail.com>
2023-07-20 05:55:03 +0530

    Second commit

---
+++ src/SampleClass.java
@@ -0,0 +1,15 @@
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+         int a=8;
+         int b=9;
+         int c=a*b;
+         System.out.println("Product is: "+c);
+
+	}
+
+}
